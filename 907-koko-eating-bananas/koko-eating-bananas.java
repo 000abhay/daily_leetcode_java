e51c04bf1cdbf4f6c111d5e7f76ce2c7c7262feb
Abhay kumar Sharma <115795726+000abhay@users.noreply.github.com>
2026-02-09 11:00:30 +0530

    Time: 9 ms (64.58%) | Memory: 47.7 MB (85.44%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -2,7 +2,7 @@ class Solution {
  static { 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> { 
             try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) { 
-                fw.write("99"); 
+                fw.write("9"); 
             } catch (Exception _) { } 
         }));
     }
