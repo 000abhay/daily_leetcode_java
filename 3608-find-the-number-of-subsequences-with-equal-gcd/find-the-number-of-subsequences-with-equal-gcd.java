41d771e09be8475ee73cd49e9cd8724722f363fd
Abhay kumar Sharma <115795726+000abhay@users.noreply.github.com>
2026-07-14 21:59:31 +0530

    Time: 275 ms (55.56%) | Memory: 206.8 MB (38.89%) - LeetSync

---
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
@@ -0,0 +1,42 @@
+class Solution {
+    int gcd(int a,int b)
+    {
+        if(b==0) return a;
+
+         return gcd(b,a%b);
+    }
+    int[][][]memo;
+    private static final int MOD=1_000_000_007;
+    int top_down(int[]nums,int i,int s1,int s2)
+    {
+        if(i==nums.length)
+        {
+            if((s1!=0&&s2!=0)&&s1==s2) return 1;
+            else return 0;
+        }
+        if(memo[i][s1][s2]!=-1) return memo[i][s1][s2];
+
+        int skip=top_down(nums,i+1,s1,s2);
+        int take_s1=top_down(nums,i+1,gcd(s1,nums[i]),s2);
+        int take_s2=top_down(nums,i+1,s1,gcd(s2,nums[i]));
+
+         long ans = (long) skip + take_s1 + take_s2;
+         memo[i][s1][s2] = (int)(ans % MOD);
+         return memo[i][s1][s2];
+
+        
+    }
+    public int subsequencePairCount(int[] nums) {
+        int n=nums.length;
+        int s=java.util.Arrays.stream(nums).max().getAsInt();
+        memo= new int[n+1][s+1][s+1];
+         for(int i=0;i<n+1;i++)
+         {
+            for(int j=0;j<s+1;j++)
+            {
+                Arrays.fill(memo[i][j],-1);
+            }
+         }
+        return top_down(nums,0,0,0);
+    }
+}
