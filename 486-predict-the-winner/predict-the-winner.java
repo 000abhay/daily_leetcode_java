cea21133d5355b6a9e1b504d03fedd8236059455
Abhay kumar Sharma <115795726+000abhay@users.noreply.github.com>
2026-08-02 10:19:17 +0530

    Time: 2 ms (37.47%) | Memory: 43 MB (10.71%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,16 +1,23 @@
 class Solution {
+    int [][] memo= new int[23][23];
     int fun(int [] nums, int l, int r)
     {
        if(l>r) return 0;
        if(l==r) return nums[l];
 
+       if(memo[l][r]!=-1) return memo[l][r];
+
        int take_l=nums[l]+Math.min(fun(nums,l+2,r),fun(nums,l+1,r-1));
        int take_r=nums[r]+Math.min(fun(nums,l+1,r-1),fun(nums,l,r-2));
 
-     return Math.max(take_l,take_r);
+     return memo[l][r]=Math.max(take_l,take_r);
 
     }
     public boolean predictTheWinner(int[] nums) {
+        for(int i=0;i<23;i++)
+        {
+            Arrays.fill(memo[i],-1);
+        }
         int total=Arrays.stream(nums).sum();
         int p1=fun(nums,0,nums.length-1);
         int p2=total-p1;
