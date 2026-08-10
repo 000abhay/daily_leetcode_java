28611196ec269990c8cb817c24dbc86979ec7caa
Abhay kumar Sharma <115795726+000abhay@users.noreply.github.com>
2026-08-10 08:33:37 +0530

    Time: 14 ms (91.38%) | Memory: 42.4 MB (83.55%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,55 +1,18 @@
 class Solution {
     public boolean winnerSquareGame(int n) {
-        boolean [][]dp= new boolean[n+1][2];
-          dp[0][0]=true;
-          dp[0][1]=false;
-
-          List<Integer>store= new ArrayList<>();
+        boolean []b1= new boolean[n+1];
           for(int i=1;i<=n;i++)
           {
-             int a=i*i;
-             if(a>n) break;
+             for(int j=1;j*j<=i;j++)
+             {
+                 int sq=j*j;
+                 if(!b1[i-sq])  
+                 {b1[i]=true;
+                      break;
+                 }
 
-             store.add(a);
+             }
           }
-        
-        for(int i=1;i<=n;i++)
-        {
-            boolean b1=true,b2=true;
-            for(int x:store)
-            {
-                if(x>i) break;
-                if(dp[i-x][1]==false&&b1)
-                {
-                    //  System.out.print("df");
-                    b1=false;
-                    dp[i][0]=false;
-                }
-
-                if(dp[i-x][0]==true&&b2)
-                {
-                    b2=false;
-                    dp[i][1]=true;
-                }
-
-                if(!b1 || !b2) break;
-                
-            }
-            if(b1)
-            {
-              //  System.out.print("df");
-                dp[i][0]=true;
-            }
-            if(b2)
-            {
-                dp[i][1]=false;
-            }
-        }
-
-          for(int i=0;i<=n;i++)
-          {
-            System.out.println("bob: " + dp[i][0] + " Alice: " + dp[i][1]);
-          }
-          return  dp[n][1];
+        return b1[n];
     }
 }
